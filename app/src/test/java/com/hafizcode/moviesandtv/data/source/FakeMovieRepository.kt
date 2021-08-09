d1d6ad19a8170c50a4d8ba46f645be36e9f79808
HafiCode02 <hafizcode02@gmail.com>
2021-08-09 09:46:54 +0700

    add test for MovieRepositoryTest.kt

---
+++ app/src/test/java/com/hafizcode/moviesandtv/data/source/FakeMovieRepository.kt
@@ -0,0 +1,152 @@
+package com.hafizcode.moviesandtv.data.source
+
+import android.util.Log
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import com.hafizcode.moviesandtv.data.DataEntity
+import com.hafizcode.moviesandtv.data.source.remote.RemoteDataSource
+import com.hafizcode.moviesandtv.data.source.remote.response.MovieResponse
+import com.hafizcode.moviesandtv.data.source.remote.response.RatedForResponse
+import com.hafizcode.moviesandtv.data.source.remote.response.TVRatedResponse
+import com.hafizcode.moviesandtv.data.source.remote.response.TVResponse
+import com.hafizcode.moviesandtv.utils.Helper.convertDate
+import com.hafizcode.moviesandtv.utils.Helper.convertMinutesToHour
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers.IO
+import kotlinx.coroutines.launch
+
+class FakeMovieRepository(private val remoteDataSource: RemoteDataSource) :
+    MovieDataSource {
+
+    private var genresData: String = ""
+    private var ratedForCertification: String = ""
+    private var playedHourTV: String = ""
+
+    override fun getMovies(): LiveData<List<DataEntity>> {
+        val listMovieResult = MutableLiveData<List<DataEntity>>()
+        CoroutineScope(IO).launch {
+            remoteDataSource.getMovies(object : RemoteDataSource.LoadMoviesCallback {
+                override fun onAllMoviesReceived(movieResponse: List<MovieResponse>) {
+                    val movieList = ArrayList<DataEntity>()
+                    for (response in movieResponse) {
+                        val movie = DataEntity(
+                            id = response.id.toString(),
+                            title = response.title.toString(),
+                            imgPoster = response.posterPath.toString()
+                        )
+                        movieList.add(movie)
+                    }
+                    listMovieResult.postValue(movieList)
+                }
+
+            })
+        }
+        return listMovieResult
+    }
+
+    override fun getMovieDetail(movieId: Int): LiveData<DataEntity> {
+        val movieDetailResult = MutableLiveData<DataEntity>()
+        CoroutineScope(IO).launch {
+            remoteDataSource.getMovieDetail(
+                movieId,
+                object : RemoteDataSource.LoadMovieDetailCallback {
+                    override fun onMovieDetailReceived(
+                        movieResponse: MovieResponse,
+                        ratedForResponse: RatedForResponse
+                    ) {
+                        movieResponse.genres?.forEachIndexed { index, data ->
+                            if (index < 1) genresData += "${data?.name}"
+                            else if (index >= 1) genresData += ", ${data?.name}"
+                        }
+                        ratedForResponse.results?.forEach {
+                            if (it?.iso31661.equals("RU", true)) {
+                                ratedForCertification =
+                                    it?.releaseDates?.get(0)?.certification.toString()
+                            }
+                        }
+                        val movie = DataEntity(
+                            movieResponse.id.toString(),
+                            movieResponse.title,
+                            movieResponse.overview,
+                            genresData,
+                            convertDate(movieResponse.releaseDate.toString()),
+                            ratedForCertification,
+                            movieResponse.voteAverage.toString(),
+                            movieResponse.runtime?.let { convertMinutesToHour(it) },
+                            movieResponse.posterPath.toString()
+                        )
+                        movieDetailResult.postValue(movie)
+                    }
+                })
+        }
+        return movieDetailResult
+    }
+
+    override fun getTVs(): LiveData<List<DataEntity>> {
+        val listTVsResult = MutableLiveData<List<DataEntity>>()
+        CoroutineScope(IO).launch {
+            remoteDataSource.getTVs(object : RemoteDataSource.LoadTvCallback {
+                override fun onAllTvShowsReceived(tvShowResponse: List<TVResponse>) {
+                    val tvsList = ArrayList<DataEntity>()
+                    for (response in tvShowResponse) {
+                        val tvs = DataEntity(
+                            id = response.id.toString(),
+                            title = response.name.toString(),
+                            imgPoster = response.posterPath.toString()
+                        )
+                        tvsList.add(tvs)
+                    }
+                    listTVsResult.postValue(tvsList)
+                }
+            })
+        }
+        return listTVsResult
+    }
+
+    override fun getTVDetail(tvId: Int): LiveData<DataEntity> {
+        val tvDetailResult = MutableLiveData<DataEntity>()
+        CoroutineScope(IO).launch {
+            remoteDataSource.getTVDetail(tvId, object : RemoteDataSource.LoadTvDetailCallback {
+                override fun onTvShowDetailReceived(
+                    tvShowResponse: TVResponse,
+                    tvRatedResponse: TVRatedResponse
+                ) {
+                    tvShowResponse.genres?.forEachIndexed { index, data ->
+                        if (index < 1) genresData += "${data?.name}"
+                        else if (index >= 1) genresData += ", ${data?.name}"
+                    }
+                    tvRatedResponse.results?.forEach {
+                        if (it?.iso31661.equals("RU", true)) {
+                            ratedForCertification =
+                                it?.rating.toString()
+                        }
+                    }
+                    tvShowResponse.episodeRunTime?.let {
+                        if (!tvShowResponse.episodeRunTime.isNullOrEmpty()) {
+                            it[0]?.let { it1 ->
+                                playedHourTV = convertMinutesToHour(
+                                    it1
+                                )
+                            }
+                        }
+                    }
+                    val tv = DataEntity(
+                        tvShowResponse.id.toString(),
+                        tvShowResponse.name,
+                        tvShowResponse.overview,
+                        genresData,
+                        convertDate(tvShowResponse.firstAirDate.toString()),
+                        ratedForCertification,
+                        tvShowResponse.voteAverage.toString(),
+                        playedHourTV,
+                        tvShowResponse.posterPath.toString()
+                    )
+                    tvDetailResult.postValue(tv)
+                }
+
+            })
+        }
+        return tvDetailResult
+    }
+
+}
---
+++ app/src/test/java/com/hafizcode/moviesandtv/data/source/MovieRepositoryTest.kt
@@ -0,0 +1,173 @@
+package com.hafizcode.moviesandtv.data.source
+
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import com.hafizcode.moviesandtv.data.source.remote.RemoteDataSource
+import com.hafizcode.moviesandtv.data.source.remote.api.ApiClient
+import com.hafizcode.moviesandtv.data.source.remote.response.MovieResponse
+import com.hafizcode.moviesandtv.data.source.remote.response.RatedForResponse
+import com.hafizcode.moviesandtv.data.source.remote.response.TVRatedResponse
+import com.hafizcode.moviesandtv.data.source.remote.response.TVResponse
+import com.hafizcode.moviesandtv.helper.LiveDataTestUtil
+import com.nhaarman.mockitokotlin2.any
+import com.nhaarman.mockitokotlin2.doAnswer
+import com.nhaarman.mockitokotlin2.eq
+import com.nhaarman.mockitokotlin2.verify
+import kotlinx.coroutines.runBlocking
+import org.junit.Assert.assertEquals
+import org.junit.Assert.assertNotNull
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+import org.mockito.Mockito
+
+class MovieRepositoryTest {
+
+    @get:Rule
+    var instantTaskExecutorRule = InstantTaskExecutorRule()
+
+    // Setup Remote and Fake Movie Repository
+    private val remote = Mockito.mock(RemoteDataSource::class.java)
+    private val movieRepository = FakeMovieRepository(remote)
+
+    // variable used to fill by response data
+    private var listMovieResponse = ArrayList<MovieResponse>() as List<MovieResponse>
+    private var listTVResponse = ArrayList<TVResponse>() as List<TVResponse>
+
+    // variable used to fill by response id from index 0 data
+    private var movieResponseId: Int = 0
+    private var tvResponseId: Int = 0
+
+    // variable used to fill by response detail from index 0 data
+    private var movieResponseDetail: MovieResponse? = null
+    private var tvsDetail: TVResponse? = null
+    private var movieRatedResponse: RatedForResponse? = null
+    private var tvRatedResponse: TVRatedResponse? = null
+
+    // Get Data Movie From API Before Start Testing
+    @Before
+    fun getMoviesTest() {
+        try {
+            ApiClient.instance.getMovies().execute().body()?.result.let {
+                if (it != null) {
+                    listMovieResponse = it
+                }
+            }
+            movieResponseId = listMovieResponse[0].id!!
+            ApiClient.instance.getDetailMovies(movieResponseId).execute().body()
+                ?.let { movie ->
+                    ApiClient.instance.getRatedForMovies(movieResponseId).execute().body()
+                        ?.let { movieRated ->
+                            movieResponseDetail = movie
+                            movieRatedResponse = movieRated
+                        }
+                }
+        } catch (e: Exception) {
+            e.printStackTrace()
+        }
+    }
+
+    // Get Data TVs From API Before Start Testing
+    @Before
+    fun getTvsTest() {
+        ApiClient.instance.getTVs().execute().body()?.result.let {
+            if (it != null) {
+                listTVResponse = it
+            }
+        }
+        tvResponseId = listTVResponse[0].id!!
+        ApiClient.instance.getTvDetail(tvResponseId).execute().body()?.let { tvs ->
+            ApiClient.instance.getRatedForTV(tvResponseId).execute().body()?.let { tvRated ->
+                tvsDetail = tvs
+                tvRatedResponse = tvRated
+            }
+        }
+    }
+
+
+    // Start Testing
+    @Test
+    fun getMovies() {
+        runBlocking {
+            doAnswer {
+                (it.arguments[0] as RemoteDataSource.LoadMoviesCallback).onAllMoviesReceived(
+                    listMovieResponse
+                )
+                null
+            }.`when`(remote).getMovies(any())
+        }
+
+        val data = LiveDataTestUtil.getValue(movieRepository.getMovies())
+
+        runBlocking {
+            verify(remote).getMovies(any())
+        }
+
+        assertNotNull(data)
+        assertEquals(listMovieResponse.size.toLong(), 20)
+    }
+
+    @Test
+    fun getMovieDetail() {
+        runBlocking {
+            doAnswer {
+                (it.arguments[1] as RemoteDataSource.LoadMovieDetailCallback).onMovieDetailReceived(
+                    movieResponseDetail!!, movieRatedResponse!!
+                )
+                null
+            }.`when`(remote).getMovieDetail(eq(movieResponseId), any())
+        }
+
+        val data =
+            LiveDataTestUtil.getValue(movieRepository.getMovieDetail(movieResponseId))
+
+        runBlocking {
+            verify(remote).getMovieDetail(eq(movieResponseId), any())
+        }
+
+        assertNotNull(data)
+        assertEquals(movieResponseDetail!!.id.toString(), data.id)
+    }
+
+    @Test
+    fun getTVs() {
+        runBlocking {
+            doAnswer {
+                (it.arguments[0] as RemoteDataSource.LoadTvCallback).onAllTvShowsReceived(
+                    listTVResponse
+                )
+                null
+            }.`when`(remote).getTVs(any())
+        }
+
+        val data = LiveDataTestUtil.getValue(movieRepository.getTVs())
+
+        runBlocking {
+            verify(remote).getTVs(any())
+        }
+
+        assertNotNull(data)
+        assertEquals(listTVResponse.size.toLong(), data.size.toLong())
+    }
+
+    @Test
+    fun getTVDetail() {
+        runBlocking {
+            doAnswer {
+                (it.arguments[1] as RemoteDataSource.LoadTvDetailCallback).onTvShowDetailReceived(
+                    tvsDetail!!, tvRatedResponse!!
+                )
+                null
+            }.`when`(remote).getTVDetail(eq(tvResponseId), any())
+        }
+
+        val data =
+            LiveDataTestUtil.getValue(movieRepository.getTVDetail(tvResponseId))
+
+        runBlocking {
+            verify(remote).getTVDetail(eq(tvResponseId), any())
+        }
+
+        assertNotNull(data)
+        assertEquals(tvsDetail!!.id.toString(), data.id)
+    }
+}
